f793d6c760bdbd0db95ac4c8622a14f5d5a30f98
samuelnfavero <faveronsamuel@gmail.com>
2022-05-10 15:31:05 -0300

    feat: Configurando  mongoDB

--- src/main/java/com/ms/cambioservice/model/Cambio.java
+++ src/main/java/com/ms/cambioservice/model/Cambio.java
@@ -1,14 +1,18 @@
 package com.ms.cambioservice.model;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+@Document("cambio")
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode
 public class Cambio implements Serializable {
 
+    @Id
     private Long id;
     private String from;
     private String to;
